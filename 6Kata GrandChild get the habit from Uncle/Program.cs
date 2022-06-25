9ecb4663b155bf123e27834c0cd8101e3e31ea29
Манютин <@DESKTOP-7BPDCOU>
2022-06-25 11:15:18 +0300

    Добавьте файлы проекта.

---
+++ 6Kata GrandChild get the habit from Uncle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6Kata_GrandChild_get_the_habit_from_Uncle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Son : Mom
+    {
+        public new string TakeTheGarbageOut()
+        {
+            return new Uncle().TakeTheGarbageOut();
+
+        }   
+
+
+    }
+}
